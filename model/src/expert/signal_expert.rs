577bc90ee491f29f5c709aed2557f2193ce02b90
Oleg <Oleg@oleg.oleg>
2026-04-18 12:11:28 +0300

    it

--- model/src/expert/signal_expert.rs
+++ model/src/expert/signal_expert.rs
@@ -7,7 +7,7 @@ use alloc::boxed::Box;
 
 const NUM_TREES: usize = 25;
 const INITIAL_THRESHOLD: f32 = 0.75;
-const TARGET_FPR: f32 = 5.0;
+const TARGET_FPR: f32 = 2.0;
 const MAX_TREE_DEPTH: usize = 20;
 
 pub struct SignalExpert {
