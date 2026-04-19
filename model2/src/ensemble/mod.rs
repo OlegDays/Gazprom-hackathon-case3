422e0bf1897ba38a097ec916fa9b647803da4933
Oleg <Oleg@oleg.oleg>
2026-04-19 19:44:02 +0300

    norm model

--- model2/src/ensemble/mod.rs
+++
@@ -1,5 +0,0 @@
-//! Модуль ансамбля экспертов
-
-mod ensemble20;
-
-pub use ensemble20::{Ensemble20, FrameResult};
--- model2/src/expert/mod.rs
+++
@@ -1,5 +0,0 @@
-//! Модуль экспертов
-
-mod signal_expert;  // ← имя файла без расширения
-
-pub use signal_expert::SignalExpert;
